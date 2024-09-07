5ccaa96a70fe16f30ae0624093092e9d6c4374e4
Manolis-V <129935802+Manolis-V@users.noreply.github.com>
2024-09-07 21:31:47 +0300

    added the persentage

---
+++ ks.js
@@ -0,0 +1,27 @@
+// Set the date for the countdown (year, month, day, hour, minute, second)
+const eventDate = new Date('2025-05-20T23:59:59').getTime();
+const startingDate = new Date('2024-05-20T23:59:59').getTime();
+
+// Update the countdown every 1 second
+const countdownFunction = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    // Time calculations
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    document.getElementById("countdown1").innerHTML = ((now - startingDate) / (eventDate - startingDate))*100 + "%";
+
+    // If the countdown is finished, write some text
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
